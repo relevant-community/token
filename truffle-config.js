06214f524ff102bf37340e15e647530ea86224ba
achill <achill_prabhat.rudolph@daimler.com>
2019-05-13 22:51:22 +0200

    Test upgradeable RelevantToken deployed on Rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
 module.exports = {
   networks: {
     coverage: {
@@ -14,6 +18,22 @@ module.exports = {
       gasPrice: 5e9,
       network_id: '*'
       // network_id: 5777
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(
+        process.env.MNEMONIC,
+        `rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+        0,
+        5
+      ),
+      network_id: 4
+    },
+    'rinkeby-test': {
+      host: 'localhost',
+      port: 9545,
+      network_id: '1004',
+      gasPrice: 10e9,
+      gas: 50000
     }
   }
 };
