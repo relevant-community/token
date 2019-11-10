cbd5b495e1348b8c083cac083d7a1958531142c3
Slava 4REAL <slava@4real.io>
2019-11-09 23:19:26 -0500

    update deps

--- truffle-config.js
+++ truffle-config.js
@@ -1,40 +1,43 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require("truffle-hdwallet-provider");
 
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   networks: {
     coverage: {
-      host: 'localhost',
+      host: "localhost",
       port: 8555,
       gas: 0xfffffffffff, // <-- Use this high gas value
       gasPrice: 0x01, // <-- Use this low gas price
-      network_id: '*'
+      network_id: "*"
     },
     local: {
-      host: 'localhost',
+      host: "localhost",
       port: 9545,
       gas: 5000000,
       gasPrice: 5e9,
-      network_id: '*'
-      // network_id: 5777
+      network_id: "*"
     },
     rinkeby: {
-      provider: () => new HDWalletProvider(
-        process.env.MNEMONIC,
-        'https://rinkeby.infura.io/' + process.env.INFURA_API_KEY
-      ),
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY,
+          2
+        ),
       network_id: 4,
       gas: 6.9e6,
-      gasPrice: 2.1e9
+      gasPrice: 3.1e9
     },
     mainnet: {
-      provider: () => new HDWalletProvider(
-        process.env.MNEMONIC,
-        'https://mainnet.infura.io/' + process.env.INFURA_API_KEY
-      ),
-      gas: 6000000,
-      gasPrice: 2e9,
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY,
+          2
+        ),
+      gas: 6e6,
+      gasPrice: 2.1e9,
       network_id: 1
     }
   }
