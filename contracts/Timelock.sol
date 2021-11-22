725cb6c12300395f6ec15ad57d6be5c3fb06bf31
balasan <slava@4real.io>
2021-11-22 14:14:38 -0500

    fix spacing

--- contracts/Timelock.sol
+++ contracts/Timelock.sol
@@ -4,10 +4,9 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts/governance/TimelockController.sol";
 
 contract RelTimelock is TimelockController {
-
- constructor(
-      uint256 minDelay,
-      address[] memory proposers,
-      address[] memory executors
+  constructor(
+    uint256 minDelay,
+    address[] memory proposers,
+    address[] memory executors
   ) TimelockController(minDelay, proposers, executors) {}
 }
