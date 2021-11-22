c63ed96e8d03c9b3a182c25cf1b9300c24a12021
balasan <slava@4real.io>
2021-11-22 14:08:24 -0500

    update gov mock spacing

--- contracts/mocks/GovernorMock.sol
+++ contracts/mocks/GovernorMock.sol
@@ -8,7 +8,6 @@ import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
 
-//spacing
 contract GovernorMock is
   Governor,
   GovernorProposalThreshold,
